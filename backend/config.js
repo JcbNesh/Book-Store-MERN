ef9a6f128b45e47c58712b2e8631aa24e6dd8c56
JcbNesh <babayqb13@gmail.com>
2024-05-23 19:13:53 +0400

    First

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://qabilmehraliyev2001:iV1jY8NM6HZ2zBzV@books-store-mern.givguff.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+
+// Please create a free database for yourself.
+// This database will be deleted after Project Finish
